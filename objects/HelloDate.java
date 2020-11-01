d3c41deb1aec7f38be8af3cd71da1ba67f7df3c4
OneCodeMonkey <1460018362@qq.com>
2020-11-01 11:35:13 +0800

    test

---
+++ objects/HelloDate.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class HelloDate {
+    public static void main(String[] args) {
+        System.out.println("The date now is:");
+        System.out.println(new Date());
+    }
+}
